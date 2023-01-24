b1134048a58aff4008fed44a37aa5d8e878914e2
mpearman15 <mpearman@uw.edu>
2023-01-23 22:32:38 -0800

    f

--- popup/popup.js
+++ popup/popup.js
@@ -1,21 +1,21 @@
-chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-  /*
-   Gets the URL from tab in focus.
-   */
-  let url = tabs[0].url;
-  chrome.storage.sync.get([url], function(items){
-      /*
-      Fetches user data(notes) from persistance storage
-       */
-      console.log((items[url]));
-      notes = items[url]
-      if(!notes)
-      {
-          notes = ""
-      }
-      document.getElementById('notes').value = notes;
-  });
-});
+// chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+//   /*
+//    Gets the URL from tab in focus.
+//    */
+//   let url = tabs[0].url;
+//   chrome.storage.sync.get([url], function(items){
+//       /*
+//       Fetches user data(notes) from persistance storage
+//        */
+//       console.log((items[url]));
+//       notes = items[url]
+//       if(!notes)
+//       {
+//           notes = ""
+//       }
+//       document.getElementById('notes').value = notes;
+//   });
+// });
 
 document.getElementById('saveButton').onclick = function(){
   /*
